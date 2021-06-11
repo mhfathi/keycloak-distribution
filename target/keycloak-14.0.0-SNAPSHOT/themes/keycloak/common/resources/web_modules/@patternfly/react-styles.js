6d19a48a94bfaee578b986426b5e6f9ee2aec5f7
Hossein Fathi <mohammadhossein.fathi@st.oth-regensburg.de>
2021-06-11 20:12:54 +0200

    Removing unnecessary files

--- target/keycloak-14.0.0-SNAPSHOT/themes/keycloak/common/resources/web_modules/@patternfly/react-styles.js
+++
@@ -1,2 +0,0 @@
-import"../common/_commonjsHelpers-7ccb6b45.js";export{S as StyleSheet,c as css,d as getClassName,b as getInsertedStyles,g as getModifier,a as isModifier,i as isValidStyleDeclaration,p as pickProperties}from"../common/StyleSheet-d3d5874a.js";
-//# sourceMappingURL=react-styles.js.map
